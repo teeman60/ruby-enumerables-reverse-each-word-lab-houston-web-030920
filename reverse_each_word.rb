ef934f5ce09a78f84ac2bf580e77c9b69db2fee7
Taiye Salami <taiye41@gmail.com>
2020-03-03 16:47:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,14 @@
-def reverse_each_word(string)
-  reversed = []
-  arr = string.split
-  arr.each do |word|
-    reversed << word.reverse
-  end
-  reversed.join(" ")
-end
-
-
 # def reverse_each_word(string)
-#
+#   reversed = []
+#   arr = string.split
+#   arr.each do |word|
+#     reversed << word.reverse
+#   end
+#   reversed.join(" ")
 # end
+
+
+def reverse_each_word(string)
+  string.split.collect { |word| word.reverse}.join(" ")
+
+end
